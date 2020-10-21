70090b026f45b26cde3e875cdac72450450fc34d
run <run@example.com>
2020-10-21 19:47:48 +0800

    fix path by cd

--- build.sh
+++ build.sh
@@ -11,6 +11,8 @@ git clone -b ${GIT_TAG} https://github.com/v2fly/v2ray-core.git
 mkdir -p /v2ray
 LDFLAGS="-s -w"
 
-env CGO_ENABLED=0 go build -o /v2ray/v2ray -trimpath -ldflags "$LDFLAGS" ./v2ray-core/main
-env CGO_ENABLED=0 go build -o /v2ray/v2ctl -trimpath -tags confonly -ldflags "$LDFLAGS" ./v2ray-core/infra/control/main
-cp v2ray-core/release/config/*.dat /v2ray/
+cd v2ray-core
+
+env CGO_ENABLED=0 go build -o /v2ray/v2ray -trimpath -ldflags "$LDFLAGS" ./main
+env CGO_ENABLED=0 go build -o /v2ray/v2ctl -trimpath -tags confonly -ldflags "$LDFLAGS" ./infra/control/main
+cp ./release/config/*.dat /v2ray/
