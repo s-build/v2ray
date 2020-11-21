add99751d46dda118aacaeacb1167c699324f4eb
Runchard <50762096+runchard@users.noreply.github.com>
2020-11-21 21:02:49 +0800

    remove arm64 support

--- build.sh
+++ build.sh
@@ -18,8 +18,3 @@ env GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -o /v2ray-darwin-amd64/v2ray
 env GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -o /v2ray-darwin-amd64/v2ctl -trimpath -tags confonly -ldflags "$LDFLAGS" ./infra/control/main
 mkdir -p /v2ray-darwin-amd64
 cp ./release/config/*.dat /v2ray-darwin-amd64/
-
-env GOOS=darwin GOARCH=arm64 CGO_ENABLED=0 go build -o /v2ray-darwin-arm64/v2ray -trimpath -ldflags "$LDFLAGS" ./main
-env GOOS=darwin GOARCH=arm64 CGO_ENABLED=0 go build -o /v2ray-darwin-arm64/v2ctl -trimpath -tags confonly -ldflags "$LDFLAGS" ./infra/control/main
-mkdir -p /v2ray-darwin-arm64
-cp ./release/config/*.dat /v2ray-darwin-arm64/
