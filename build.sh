3ded284920a1cea29fef45b4eb86e7b3b684ff2e
runchard <50762096+runchard@users.noreply.github.com>
2020-11-21 21:51:35 +0800

    generate darwin{arm64,amd64} artifacts (#7)
    * generate darwin and arm64 artifacts
    
    * remove arm64 support

--- build.sh
+++ build.sh
@@ -5,11 +5,16 @@ set -ex
 GIT_TAG="v4.33.0"
 git clone -b ${GIT_TAG} https://github.com/v2fly/v2ray-core.git
 
-mkdir -p /v2ray
 LDFLAGS="-s -w"
 
 cd v2ray-core
 
 env CGO_ENABLED=0 go build -o /v2ray/v2ray -trimpath -ldflags "$LDFLAGS" ./main
 env CGO_ENABLED=0 go build -o /v2ray/v2ctl -trimpath -tags confonly -ldflags "$LDFLAGS" ./infra/control/main
+mkdir -p /v2ray
 cp ./release/config/*.dat /v2ray/
+
+env GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -o /v2ray-darwin-amd64/v2ray -trimpath -ldflags "$LDFLAGS" ./main
+env GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -o /v2ray-darwin-amd64/v2ctl -trimpath -tags confonly -ldflags "$LDFLAGS" ./infra/control/main
+mkdir -p /v2ray-darwin-amd64
+cp ./release/config/*.dat /v2ray-darwin-amd64/
