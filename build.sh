fd0680632733a22fedb1285d40148905fc463af4
Runchard <50762096+runchard@users.noreply.github.com>
2020-12-02 20:53:11 +0800

    smaller logic change range

--- build.sh
+++ build.sh
@@ -8,13 +8,12 @@ git clone -b ${GIT_TAG} https://github.com/v2fly/v2ray-core.git
 LDFLAGS="-s -w"
 
 cd v2ray-core
-pwd
 
-for GOOS_GOARCH in $data
-do
+while IFS=' ' read -r GOOS_GOARCH; do
     while IFS='-' read -r GOOS GOARCH; do
         env
-        go build -o /v2ray-darwin-amd64/v2ray -trimpath -ldflags "$LDFLAGS" ./main
-        go build -o /v2ray-darwin-amd64/v2ctl -trimpath -tags confonly -ldflags "$LDFLAGS" ./infra/control/main
-    done <<< $part
+        go build -o /v2ray-${GOOS_GOARCH}/v2ray -trimpath -ldflags "$LDFLAGS" ./main
+        go build -o /v2ray-${GOOS_GOARCH}/v2ctl -trimpath -tags confonly -ldflags "$LDFLAGS" ./infra/control/main
+        cp ./release/config/*.dat /v2ray-${GOOS_GOARCH}/
+    done <<< $GOOS_GOARCH
 done <<< $GOOS_GOARCHS
