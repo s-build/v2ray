f3ee377c497bd4582bc2d92a48439827d6a36732
Runchard <50762096+runchard@users.noreply.github.com>
2020-12-02 20:55:58 +0800

    debug with a env

--- build.sh
+++ build.sh
@@ -9,11 +9,12 @@ LDFLAGS="-s -w"
 
 cd v2ray-core
 
+env
 while IFS=' ' read -r GOOS_GOARCH; do
     while IFS='-' read -r GOOS GOARCH; do
         env
         go build -o /v2ray-${GOOS_GOARCH}/v2ray -trimpath -ldflags "$LDFLAGS" ./main
         go build -o /v2ray-${GOOS_GOARCH}/v2ctl -trimpath -tags confonly -ldflags "$LDFLAGS" ./infra/control/main
         cp ./release/config/*.dat /v2ray-${GOOS_GOARCH}/
-    done <<< $GOOS_GOARCH
-done <<< $GOOS_GOARCHS
+    done <<< ${GOOS_GOARCH}
+done <<< ${GOOS_GOARCHS}
