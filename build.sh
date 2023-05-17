2e82a96e63c665c8df1daa4cd810681180debaf2
Runchard <50762096+runchard@users.noreply.github.com>
2023-05-17 11:36:34 +0800

    no longer need v2ctl in v5
    ref b05a46948804c4def17e8c7829da7ddec2368b53

--- build.sh
+++ build.sh
@@ -15,7 +15,6 @@ for GOOS_GOARCH in ${GOOS_GOARCHS}; do
         export GOARCH=$GOARCH
         export CGO_ENABLED=0
         go build -o /v2ray-${GOOS_GOARCH}/v2ray -trimpath -ldflags "$LDFLAGS" ./main
-        go build -o /v2ray-${GOOS_GOARCH}/v2ctl -trimpath -tags confonly -ldflags "$LDFLAGS" ./infra/control/main
         wget -O release/config/geoip.dat "https://raw.githubusercontent.com/v2fly/geoip/release/geoip.dat"
         wget -O release/config/geosite.dat "https://raw.githubusercontent.com/v2fly/domain-list-community/release/dlc.dat"
         cp ./release/config/*.dat /v2ray-${GOOS_GOARCH}/
